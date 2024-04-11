d6b709f3222ec775ab9e293dbbf095b1babf95e9
bang-dev <ecosystemprogramminglanguage@gmail.com>
2024-04-12 02:50:09 +0700

    Update

---
+++ mysource/src/main/java/com/practice/mysource/MySourceApplication.java
@@ -0,0 +1,13 @@
+package com.practice.mysource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySourceApplication.class, args);
+	}
+
+}
---
+++ mysource/src/test/java/com/practice/mysource/MySourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.mysource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySourceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
